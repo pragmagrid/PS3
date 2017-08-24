d88d7432f6bff3b1b653eec0eede91d342d8e6df
nadyawilliams <nadya@sdsc.edu>
2017-08-24 13:00:15 -0700

    make rpm from packaged s3 distro. Keep distro on google drive

--- src/ps3/version.mk
+++ src/ps3/version.mk
@@ -1,6 +1,8 @@
 PKGROOT	   = /usr/lib/node_modules/s3
-NAME       = ps3-placeholder
-REALNAME   = ps3
+NAME       = ps3
+DISTRONAME = S3
 VERSION    = 1.0
-RELEASE    = 1
+RELEASE    = 0
+TARBALL_POSTFIX = tar.gz
+
 
